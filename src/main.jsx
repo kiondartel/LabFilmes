6599b13b3caefc2c8879a271b35e4eb3c99b879f
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-14 16:32:00 -0300

    Routes Config

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes } from "react-router-dom";
 import Home from "./home/Home";
 import Global from "./styles/globalstyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />}>
+          <Route path="/" element={<App />} />
+          <Route path="/movie/:id" element={<Movies />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <Global />
   </React.StrictMode>
 );
